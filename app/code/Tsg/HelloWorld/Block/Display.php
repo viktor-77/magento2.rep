ee23c64569ae33e248786eaf674ad98d083b8a1d
vikto-77 <vumanskiy0664041341@gmail.com>
2022-08-23 11:20:48 +0300

    first commit

---
+++ app/code/Tsg/HelloWorld/Block/Display.php
@@ -0,0 +1,14 @@
+<?php
+namespace Tsg\HelloWorld\Block;
+class Display extends \Magento\Framework\View\Element\Template
+{
+	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+	{
+		parent::__construct($context);
+	}
+
+	public function sayHello()
+	{
+		return __('hi in Hello World <i>display</i> block');
+	}
+}
---
+++ app/code/Tsg/HelloWorld/Block/Index.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Tsg\HelloWorld\Block;
+class Index extends \Magento\Framework\View\Element\Template
+{
+
+}
